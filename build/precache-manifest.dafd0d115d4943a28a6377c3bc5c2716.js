ea41e31b95ec07533470029695770bfcb5913885
mohammadrezaei0123 <53704454+mohammadrezaei0123@users.noreply.github.com>
2019-09-02 10:31:13 -0700

    Add files via upload
    upluad app

---
+++ build/precache-manifest.dafd0d115d4943a28a6377c3bc5c2716.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d0d63f504ee4bc381cdda1d1ade0e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70206a3ce12cb1c3ea4",
+    "url": "/static/css/2.a6b2473e.chunk.css"
+  },
+  {
+    "revision": "f87ffdb686fbfd66c4e4",
+    "url": "/static/css/main.fd6d903f.chunk.css"
+  },
+  {
+    "revision": "c70206a3ce12cb1c3ea4",
+    "url": "/static/js/2.ac1c19c8.chunk.js"
+  },
+  {
+    "revision": "f87ffdb686fbfd66c4e4",
+    "url": "/static/js/main.5c2f583d.chunk.js"
+  },
+  {
+    "revision": "0769ca731d2483dac950",
+    "url": "/static/js/runtime~main.07faa16a.js"
+  },
+  {
+    "revision": "a34037e528411213bd8246a74448f496",
+    "url": "/static/media/loadImage.a34037e5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
